8de2858b6c7e83a395caa7b7f063ac9d36326064
r-angeles <rae.m.angeles@gmail.com>
2023-02-13 23:18:17 +1300

    removed svg support

--- .eleventy.js
+++ .eleventy.js
@@ -2,18 +2,8 @@ const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const Image = require("@11ty/eleventy-img");
 const htmlmin = require("html-minifier");
-const svgSprite = require("eleventy-plugin-svg-sprite");
 
 module.exports = function(eleventyConfig) {
-
-    // Inlining SVG
-    eleventyConfig.addPlugin(svgSprite, {
-        defaultClasses: "w-5 h-5 fill-current",
-        path: "./src/_includes/icons", // relative path to SVG directory
-        svgSpriteShortcode: "svgsprite",
-        svgShortcode: "svg",
-    });
-        
     // Human readable date
     eleventyConfig.addFilter("readableDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
